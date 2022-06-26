622c794c1db7c1a048fb2b126c91c334135b051f
LeVi4869 <thanhvinhle3900@gmail.com>
2022-06-26 21:48:58 +0700

    creating first app

---
+++ MyFirstApp/app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.myfirstapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val text = findViewById<TextView>(R.id.textView)
+        val btnClickMe = findViewById<Button>(R.id.mybutton)
+        var peopleCount = 0
+        btnClickMe.setOnClickListener {
+            ++peopleCount
+            text.text = peopleCount.toString()
+        }
+    }
+}
