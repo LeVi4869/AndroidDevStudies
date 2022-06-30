572f5f47f500116960aea92f355ce7b88303cff7
LeVi4869 <thanhvinhle3900@gmail.com>
2022-06-30 21:58:47 +0700

    finishing Age In Minutes app

--- DOBCalcByMinutes/app/src/main/java/com/example/dobcalcbyminutes/MainActivity.kt
+++ DOBCalcByMinutes/app/src/main/java/com/example/dobcalcbyminutes/MainActivity.kt
@@ -1,11 +1,59 @@
 package com.example.dobcalcbyminutes
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var tvSelectedDate : TextView? = null
+    private var tvAgeInMinutes : TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        tvSelectedDate = findViewById(R.id.tvSelectedDate)
+        tvAgeInMinutes = findViewById(R.id.tvAgeInMinutes)
+        val btnDatePicker : Button = findViewById(R.id.btnDatePicker)
+        btnDatePicker.setOnClickListener {
+            clickDatePicker()
+        }
+    }
+
+    fun clickDatePicker()   {
+        val myCalendar = Calendar.getInstance()
+        val year = myCalendar.get(Calendar.YEAR)
+        val month = myCalendar.get(Calendar.MONTH)
+        val day = myCalendar.get(Calendar.DAY_OF_MONTH)
+        DatePickerDialog(this, { view, selectedYear, selectedMonth, selectedDayOfMonth ->
+
+            val selectedDate = "$selectedDayOfMonth/${selectedMonth + 1}/$selectedYear"
+            tvSelectedDate?.setText(selectedDate)
+
+            val sdf = SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH)
+            val theDate = sdf.parse(selectedDate)
+
+            val selectedDateInMinutes = theDate.time / 60000
+
+            val currentDate = sdf.parse(sdf.format(System.currentTimeMillis()))
+
+            val currentDateInMinutes = currentDate.time / 60000
+
+            val minuteDifference = currentDateInMinutes - selectedDateInMinutes
+
+            if(minuteDifference < 0)    {
+                Toast.makeText(this, "Selected date must be before today", Toast.LENGTH_LONG).show()
+            }
+            else {
+                tvAgeInMinutes?.setText(minuteDifference.toString())
+                Toast.makeText(this, "Success", Toast.LENGTH_LONG).show()
+            }
+        },
+        year, month, day).show()
     }
 }
