e40e6448e2c29f298c3102544f29807f30f2febf
LeVi4869 <thanhvinhle3900@gmail.com>
2022-08-09 18:51:34 +0700

    finishing the exercise activity display and the app

--- 7MinWorkout/app/src/main/java/com/example/a7minworkout/HistoryDao.kt
+++ 7MinWorkout/app/src/main/java/com/example/a7minworkout/HistoryDao.kt
@@ -2,9 +2,14 @@ package com.example.a7minworkout
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface HistoryDao {
     @Insert
     suspend fun insert(historyEntity : HistoryEnitity)
+
+    @Query("SELECT * FROM `history-table`")
+    fun fetchAllDates() : Flow<List<HistoryEnitity>>
 }
